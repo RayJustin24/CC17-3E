01f9bfda2f092e08630bb4de501091577bff73e5
RayJustin24 <villanuevarayjustin44@gmail.com>
2023-11-08 21:27:23 +0800

    Login Page

---
+++ LabAct3/BirthdayGreeting.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val age = 20
+
+    printBirthdayGreetings(age)
+
+}
+
+fun printBirthdayGreetings(age: Int) {
+    println("Happy Birthday!")
+    println("You are $age years old.")
+    println("Enjoy your special day!\n")
+}
